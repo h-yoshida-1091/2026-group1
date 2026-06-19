08b55c2824c0484b6b4eae0d1d9f7eedd11ce01b
h-yoshida-1091 <h.yoshida@tbc-inc.co.jp>
2026-06-19 17:06:51 +0900

    OrderController実装中 購入確認画面で表示するデータの取得の処理を書いたが実行確認はできていない

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,16 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 
 // 商品一覧
 Route::get('/products', [ProductController::class, 'index']);
 
 // 商品詳細
 Route::get('/products/detail', [ProductController::class, 'show']);
+
+// 購入確認
+Route::get('/purchase', [OrderController::class, 'order']);
+
+//　購入完了
+Route::get('/purchase/complete', [OrderController::class, 'complete']);
