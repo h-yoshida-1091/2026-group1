8caddaad1dc4ff798273ef77839e4a7139dfaac5
Atsumu Niino <715-student009@la-study.com>
2026-06-25 15:43:18 +0900

    商品一覧の赤線エラーを修正

--- resources/views/products/lineup.blade.php
+++ resources/views/products/lineup.blade.php
@@ -97,8 +97,9 @@
                 @foreach($priceRanges as $range)
                 <li>
                     <a href="javascript:void(0)"
-                        onclick="clickPriceRange({{ $range['min'] }}, {{ $range['max'] }})"
-                        class="price-range-link {{ (request('min_price') == $range['min'] && request('max_price') == $range['max']) ? 'active' : '' }}">
+                        data-min="{{ $range['min'] }}"
+                        data-max="{{ $range['max'] }}"
+                        class="price-range-link price-filter {{ (request('min_price') == $range['min'] && request('max_price') == $range['max']) ? 'active' : '' }}">
                         {{ $range['label'] }}
                     </a>
                 </li>
@@ -131,7 +132,7 @@
                     </div>
                     @endif
 
-                    <a href="/products/detail?id={{ $product->id }}" >
+                    <a href="/products/detail?id={{ $product->id }}">
                         <img src="{{ $product->image_url }}" alt="{{ $product->name }}" class="{{ $product->stock === 0 ? 'img-soldout' : '' }}">
                     </a>
                 </div>
