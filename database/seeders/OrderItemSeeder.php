44b66e5de356043856d7171b345233ad2fa54b8d
h-yoshida-1091 <h.yoshida@tbc-inc.co.jp>
2026-06-26 12:02:09 +0900

    OrderSeeder.phpとOrderItemSeeder.phpの作成

---
+++ database/seeders/OrderItemSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class OrderItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //データリセット
+        DB::table('order_items')->truncate();
+
+        //データ挿入
+        DB::table('order_items')->insert([
+            [
+                'order_id' => 1,
+                'product_id' => 1,
+                'quantity' => 2,
+                'price' => 2500
+            ],
+            [
+                'order_id' => 1,
+                'product_id' => 2,
+                'quantity' => 4,
+                'price' => 500
+            ],
+        ]);
+
+    }
+}
+             
